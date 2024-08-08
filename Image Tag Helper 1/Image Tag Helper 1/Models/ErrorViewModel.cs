f9901809182cc53b2ec577ca905163549ce895a7
MohammadAmmad <117718535+Ammadgithub123@users.noreply.github.com>
2024-08-08 15:55:27 +0500

    Initial commit

---
+++ Image Tag Helper 1/Image Tag Helper 1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Image_Tag_Helper_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
